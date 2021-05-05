5250d363e8726a78854c0aac7459793252699bd4
yue qi <391311qy@gmail.com>
2021-05-04 20:55:54 -0400

    added card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Card from "./components/Card"
 
 function App() {
   return (
     <div className="App">
         Movie List
+        <Card/>
     </div>
   );
 }
---
+++ src/components/BlockList.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class BlockList extends React.Component {
+    render() {
+        return (
+            <>  
+                {/* <NavBar/> */}
+                <div className = "blockList">
+                
+                </div> 
+            </>
+        )
+    }
+}
